9406d3ffb07b78c0ee212abd8c53a74a73b3eb7e
Nicholas Taylor <nicholas_taylor@me.com>
2021-01-23 23:46:45 -0500

    Rearranging site structure

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,50 +1,49 @@
 import React from "react"
-import { graphql } from "gatsby"
-import Card from "../components/Card"
+import { Link } from "gatsby"
 import "../styles/bootstrap-reboot.min.css"
 import "../styles/site.scss"
 
-export default function home({ data }) {
+export default function home() {
   return (
     <div
       className="l-container"
     >
-      {data.allMarkdownRemark.edges.map(({ node }, index) => (
-        <Card
-          key={index}
-          name={node.frontmatter.title}
-          desc={node.rawMarkdownBody}
-          type={node.frontmatter.type}
-          btnLiveCopy={node.frontmatter.live_text}
-          btnLiveHref={node.frontmatter.live_uri}
-          btnGitCopy={node.frontmatter.gh_text}
-          btnGitHref={node.frontmatter.gh_uri}  
-          tags={node.frontmatter.tags}  
-          priority={node.frontmatter.priority}         
-        />
-      ))}    
+        <div
+            className="c-mainmenu"
+        >
+            <div
+                className="c-mainmenu__headline"
+            >
+                Hi! I'm Nick!
+            </div>
+            <div
+                className="c-mainmenu__link"
+            >
+                <Link
+                    to="/works/"
+                >
+                    Projects
+                </Link>
+            </div>
+            <div
+                className="c-mainmenu__link"
+            >
+                <Link
+                    to="/works/"
+                >
+                    About Me
+                </Link>
+            </div>
+            <div
+                className="c-mainmenu__link"
+            >
+                <Link
+                    to="/works/"
+                >
+                    Contact
+                </Link>                                
+            </div>
+        </div>
     </div>
   )
 }
-
-export const query = graphql`
-query MyQuery {
-  allMarkdownRemark(sort: {fields: frontmatter___priority, order: ASC}) {
-    edges {
-      node {
-        rawMarkdownBody
-        frontmatter {
-          title
-          type
-          live_uri
-          live_text
-          gh_uri
-          gh_text
-          priority
-          tags
-        }
-      }
-    }
-  }
-}
-`
---
+++ src/pages/works.js
@@ -0,0 +1,50 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Card from "../components/Card"
+import "../styles/bootstrap-reboot.min.css"
+import "../styles/site.scss"
+
+export default function home({ data }) {
+  return (
+    <div
+      className="l-container"
+    >
+      {data.allMarkdownRemark.edges.map(({ node }, index) => (
+        <Card
+          key={index}
+          name={node.frontmatter.title}
+          desc={node.rawMarkdownBody}
+          type={node.frontmatter.type}
+          btnLiveCopy={node.frontmatter.live_text}
+          btnLiveHref={node.frontmatter.live_uri}
+          btnGitCopy={node.frontmatter.gh_text}
+          btnGitHref={node.frontmatter.gh_uri}  
+          tags={node.frontmatter.tags}  
+          priority={node.frontmatter.priority}         
+        />
+      ))}    
+    </div>
+  )
+}
+
+export const query = graphql`
+query MyQuery {
+  allMarkdownRemark(sort: {fields: frontmatter___priority, order: ASC}) {
+    edges {
+      node {
+        rawMarkdownBody
+        frontmatter {
+          title
+          type
+          live_uri
+          live_text
+          gh_uri
+          gh_text
+          priority
+          tags
+        }
+      }
+    }
+  }
+}
+`
